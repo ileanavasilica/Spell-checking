56153ac8ee351296b9f4a515bd97b3d8c6975d65
ileanavasilica <41501733+ileanavasilica@users.noreply.github.com>
2020-01-14 21:15:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,52 +9,56 @@ int main()
 {
 	SpellChecker a;
 	string cuvant, prefix; 
-	string nume_fisier; 
+	char nume_fisier[30]; 
 	string s; 
 	int option=0;
 	cout << "Introduceti numele fisierului (dictionary.txt): ";
 	cin >> nume_fisier; 
 	ifstream fin(nume_fisier);
-	cout << "Continutul dictionarului este:" ;
-	
-	while (option != 2)
+	if (strcmp(nume_fisier, "dictionary.txt") != 0)
+		cout << "Ati gresit numele fisierului";
+	else
 	{
-		while (fin >> s)
+		cout << "Continutul dictionarului este:";
+		while (option != 2)
 		{
-			a.insert(s);
+			while (fin >> s)
+			{
+				a.insert(s);
 
-			cout << s << " ";
-		}
-		cout << endl; 
-		cout << "-----------------------------";
-		cout << endl; 
-		cout << "Meniul :" << endl;
-		cout << "1.Doriti sa cautati un cuvant intreg "<<endl;
-		cout << "2.Doriti sa cautati un prefix "<<endl;
-		cout << "Alegerea dumneavoastra este: ";
-		cin >> option; 
-		cout << endl; 
-		switch (option)
-		{
-		case 1: //doriti sa cautati un cuvant
-			cout << "Introduceti cuvantul pe care doriti sa - l cautati:";
-			cin >> cuvant; 
-			if (a.search(cuvant) == 0)
-				cout << "Cuvantul nu exista in dictionar.";
-			else
-				cout << "Yeeeeeey!!! Cuvantul exista in dictionar.";
-			break; 
-		case 2: //doriti sa cautati un prefix
-			cout<< "Introduceti prefixul pe care doriti sa - l cautati:";
-			cin >> prefix; 
-			if (a.startsWith(prefix) == 0)
-				cout << "Nu exista cuvinte in dictionar cu prefixul :" << prefix << endl;
-			else
-				cout << "Exista cuvinte in dictionar cu prefixul introdus ! "; 
-			break;
-		default:
-			cout << "EROARE";
-			break;
+				cout << s << " ";
+			}
+			cout << endl;
+			cout << "-----------------------------";
+			cout << endl;
+			cout << "Meniul :" << endl;
+			cout << "1.Doriti sa cautati un cuvant intreg " << endl;
+			cout << "2.Doriti sa cautati un prefix " << endl;
+			cout << "Alegerea dumneavoastra este: ";
+			cin >> option;
+			cout << endl;
+			switch (option)
+			{
+			case 1: //doriti sa cautati un cuvant
+				cout << "Introduceti cuvantul pe care doriti sa - l cautati:";
+				cin >> cuvant;
+				if (a.search(cuvant) == 0)
+					cout << "Cuvantul nu exista in dictionar.";
+				else
+					cout << "Yeeeeeey!!! Cuvantul exista in dictionar.";
+				break;
+			case 2: //doriti sa cautati un prefix
+				cout << "Introduceti prefixul pe care doriti sa - l cautati:";
+				cin >> prefix;
+				if (a.startsWith(prefix) == 0)
+					cout << "Nu exista cuvinte in dictionar cu prefixul :" << prefix << endl;
+				else
+					cout << "Exista cuvinte in dictionar cu prefixul introdus ! ";
+				break;
+			default:
+				cout << "EROARE";
+				break;
+			}
 		}
 	}
 	return 0;
